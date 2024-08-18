d8de6104c8ecf8ef08812a538f5800dce51be4ac
Mamdoh-Zeyad <mamdohzx@gmail.com>
2024-08-18 13:26:34 +0300

    Add project files.

---
+++ ProductMicroservicesProject/Repository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using ProductMicroservicesProject.Models;
+
+namespace ProductMicroservicesProject.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductById(int id);
+        void InsertProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(int id);
+        void Save();
+
+    }
+}
---
+++ ProductMicroservicesProject/Repository/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using ProductMicroservicesProject.Models;
+
+namespace ProductMicroservicesProject.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _dbcontext;
+        public ProductRepository(AppDbContext context) { _dbcontext = context; }
+        public void DeleteProduct(int id)
+        {
+            var product = _dbcontext.Products.Find(id);
+            _dbcontext.Products.Remove(product);
+            Save();
+        }
+
+        public Product GetProductById(int id)
+        {
+            return _dbcontext.Products.Find(id);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _dbcontext.Products.ToList();
+        }
+
+        public void InsertProduct(Product product)
+        {
+            _dbcontext.Add(product);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbcontext.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _dbcontext.Entry(product).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            Save();
+        }   
+    }
+}
